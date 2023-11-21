8d811d21844b85a159866e711eaf817df58e3369
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2023-11-21 17:18:23 -0600

    feat: add header search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
-import Image from 'next/image'
+import Link from 'next/link'
+
 import Construction from './ui/Construction'
 import InputSearch from './ui/InputSearch'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-merme-cream-bg dark:bg-merme-black">
-      <Construction />
-      <InputSearch />
-    </main>
+    <>
+      <header className='flex justify-between items-center h-24 px-8 bg-merme-cream-bg dark:bg-merme-black border-b-4 border-merme-black dark:border-merme-cream-bg'>
+        <Link href='/' className='flex justify-center items-center font-bold text-2xl text-merme-black dark:text-merme-cream-bg'>
+          <span>LOGO</span>
+          <h1 className='ml-2'>MermeTasker</h1>
+        </Link>
+        <div className='flex items-center gap-3'>
+          <InputSearch />
+          <button className="flex justify-center items-center bg-merme-orange hover:bg-orange-400 w-72 h-12 dark:bg-merme-skyblue border-2 border-merme-black dark:border-merme-cream-bg text-xl text-merme-cream-bg dark:text-merme-black font-semibold rounded-md transition-colors duration-300">
+            <svg className="w-6 h-6 text-2xl mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+            </svg>
+            <p>Crear tablero</p>
+          </button>
+        </div>
+        <div className="relative w-10 h-10 overflow-hidden bg-merme-black dark:bg-merme-cream-bg rounded-full ">
+          <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
+        </div>
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-merme-cream-bg dark:bg-merme-black">
+        <Construction />
+      </main>
+    </>
   )
 }
--- src/app/ui/InputSearch.tsx
+++ src/app/ui/InputSearch.tsx
@@ -3,7 +3,7 @@ const InputSearch = () => {
     <div className="flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-merme-black dark:bg-merme-cream-bg  overflow-hidden">
       <div className="grid place-items-center h-full w-12 text-gray-300">
         <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
       </div>
       <input
